5f4b162b9296c65d2bacbf6e491a0c99dd04f1a1
yaroslav <yaosobolev@gmail.com>
2023-01-17 22:00:54 +0500

    прога 5

---
+++ example005/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите имя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower()=="анна"){
+    Console.Write("Здравствуй, моя любимая "+username);
+
+}
+else Console.Write("ку, "+username);
